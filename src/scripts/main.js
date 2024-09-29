b61b2bd2096a1d82bf0599304c1c3218f1527f68
kaiwitek <148138592+kaiwitek@users.noreply.github.com>
2024-09-29 14:49:28 -0300

    Delete src directory

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('mensagem').addEventListener('submit', function(e){
-        e.preventDefault()
-        let textoU = document.getElementById('texto').value 
-        let sMensagem = document.getElementById('sMensagem')
-        
-        sMensagem.innerHTML = textoU
-    })
-})
